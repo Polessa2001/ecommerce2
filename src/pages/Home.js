f4167780902c2854de5070bc02f8e1c459e21c64
Polina Sokolova <polina2001sokolovas@gmail.com>
2024-02-11 20:42:46 +0100

    set products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { ProductContext } from '../contexts/ProductContext';
 import CartItem from '../components/CartItem';
-
+import Product from '../components/Product'
 const Home = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
@@ -13,7 +13,7 @@ const Home = () => {
 
   console.log(products);
   const filteredProducts = products.data.filter((item) => {
-    return item.category.name === "men's fashion" || item.category.name === "women's fashion";
+    return item.category.name !=="tools & hardware" && item.category.name !=="phone & tablets";
   });
   console.log(filteredProducts);
 
@@ -21,11 +21,9 @@ const Home = () => {
     <div>
       <section className="py-16">
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts.map((product, index) => (
-              <div className="w-full h-[300px] bg-pink-200 mb-4" key={product.id || index}>
-                {product.title}
-              </div>
+             <Product key={product._id}/>
             ))}
           </div>
         </div>
